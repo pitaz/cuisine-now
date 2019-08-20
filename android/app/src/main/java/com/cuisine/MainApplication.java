1e9448f951b8366dd11284d38f14068a1f2d949e
pitaz <peter.odekwo@gmail.com>
2019-08-20 23:37:38 +0100

    feat(bottom-dialog): add bottom action sheet
    - create bottom sliding dialog
    
    [Finishes #168015785]

--- android/app/src/main/java/com/cuisine/MainApplication.java
+++ android/app/src/main/java/com/cuisine/MainApplication.java
@@ -9,6 +9,7 @@ import com.facebook.react.bridge.JavaScriptExecutorFactory;
 import com.facebook.react.ReactApplication;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import ui.bottomactionsheet.RNBottomActionSheetPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
