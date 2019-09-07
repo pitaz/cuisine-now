703344fa7c290f597e7f9dbe80a522185c9f3275
pitaz <peter.odekwo@gmail.com>
2019-09-07 21:19:05 +0100

    feat(google-map): add map view
    - add map view
    
    [Finishes #168346154]

--- src/components/screens/Menu/index.js
+++ src/components/screens/Menu/index.js
@@ -6,7 +6,7 @@ import MenuItem from './MenuItem';
 const Menu = ({ navigation }) => {
 
 	return (
-		<ScrollView style={styles.ScrollViewStyle}>
+		<ScrollView>
 			{
 				menu.map((item, index) => (
 					<MenuItem key={index} navigation={navigation} menuData={item} />
@@ -15,51 +15,5 @@ const Menu = ({ navigation }) => {
 		</ScrollView>
 	);
 };
-
-const styles = StyleSheet.create({
-	wrapper: {
-		height: 200
-	},
-	ScrollViewStyle: {
-		// marginTop: 60
-	},
-	slide: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
-	slide2: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#97CAE5'
-	},
-	slide3: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#92BBD9'
-	},
-	text: {
-		position: 'absolute',
-		color: '#fff',
-		fontSize: 30,
-		fontWeight: '400',
-		marginTop: 20,
-		zIndex: 100,
-		fontFamily: 'Raleway-Black'
-	},
-	restaurantsImageStyle: {
-		flex: 1, 
-		height: 200, 
-		width: '100%'
-	},
-	restaurantsCountText: {
-		marginLeft: 5, 
-		padding: 12, 
-		fontSize: 14, 
-		fontWeight: '400'
-	}
-});
 
 export default Menu;
