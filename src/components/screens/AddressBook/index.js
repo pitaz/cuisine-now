b7bce92470f495282e205c845d1d744023df315a
pitaz <peter.odekwo@gmail.com>
2019-09-12 16:17:39 +0100

    feat(address-book): add address book screen
    - refactor address book component
    
    [Finishes #168394558]

--- src/components/screens/AddressBook/index.js
+++ src/components/screens/AddressBook/index.js
@@ -1,28 +1,58 @@
 import React from 'react';
 import { View, ScrollView, StyleSheet, Text } from 'react-native';
-import { Input, Button, Card } from 'react-native-elements';
+import { Input, Button, Card, Icon } from 'react-native-elements';
 import { Section } from '../../common';
 
 const AddressBook = () => {
 	return (
-		<ScrollView style={styles.scrollView}>
-			<View style={styles.main}>
+		<View style={styles.main}>
+			<ScrollView>
 				<Card style={styles.card}>
-					<View>
-						<Text>Address book</Text>
+					<View style={styles.cardContentContainer}>
+						<View style={styles.addressBookView}>
+							<Text style={styles.label}>Peter Odekwo</Text>
+							<Text style={styles.label}>1 Aminu Street Mende Maryland</Text>
+							<Text style={styles.label}>Lagos</Text>
+							<Text style={styles.label}>Lagos, 100211</Text>
+							<Text style={styles.label}>Nigeria</Text>
+							<Text style={styles.label}>08123456789</Text>
+						</View>
+						<View>
+							<Icon
+								name='pencil'
+								type='font-awesome'
+								iconStyle={styles.icon} />
+							<Icon
+								name='trash'
+								type='font-awesome'
+								color='#C7CAD1'
+								iconStyle={styles.icon} />
+						</View>
+					</View>
+					<View style={styles.cardFooter}>
+						<Text style={styles.label2}>This your default delivery address</Text>
+						<Text style={styles.label2}>This your default billing address</Text>
 					</View>
 				</Card>
+			</ScrollView>
+			<View style={styles.pageFooter}>
+				<Text style={styles.pageFooterText}>Add new address</Text>
 			</View>
-		</ScrollView>
+			
+		</View>
+
 	);
 };
 
 const styles = StyleSheet.create({
-	section: {
-		// padding: 5,
-	},
 	label: {
-		fontSize: 11
+		fontSize: 11,
+		padding: 5
+	},
+	label2: {
+		fontSize: 11,
+		padding: 5,
+		color: '#C7CAD1'
 	},
 	card: {
 		padding: 0, 
@@ -42,11 +72,33 @@ const styles = StyleSheet.create({
 		backgroundColor: '#F5F6F9'
 	},
 	main: {
-		margin: 5
+		flex: 1,
+		justifyContent: 'space-between',
+		backgroundColor: '#F5F6F9'
 	},
 	button: {
 		margin: 20, 
 		backgroundColor: 'gray'
+	},
+	cardContentContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	},
+	icon: {
+		padding: 10
+	},
+	addressBookView: {
+		width: 220
+	},
+	cardFooter: {
+		marginTop: 10
+	},
+	pageFooter: {
+		backgroundColor: '#C7CAD1'
+	},
+	pageFooterText: {
+		textAlign: 'center', 
+		padding: 20
 	}
 });
 
